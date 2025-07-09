8fda13b17d886c0615f47c4fd480aab3c21e1a19
strsryw <satrio.suryo.wibowo@gmail.com>
2025-07-09 10:24:35 +0700

    update rabu pagi bosq
    relate to #62

--- controllers/txtkonflik.php
+++ controllers/txtkonflik.php
@@ -1,8 +1,3 @@
 <?php
 
-echo "konflik development";
-echo "konflik asik";
-echo "coba dari gitbes";
-echo "test lagi development";
-echo "test bosss";
-echo "coba lagi deh lah woi";
+echo "test rabu pagi";
